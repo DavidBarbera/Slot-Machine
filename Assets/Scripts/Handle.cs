d573d02142215facfe6614b59200783925535009
David Barbera <davidbarbera9@gmail.com>
2016-01-29 18:16:45 +0000

    Final Version
    all basic functionality: spinning the wheels, handle movement, animation
    and a cheat mechanism
    We had to repost the project this new repository because we had too many
    problems merging pull requests in our clone one.

---
+++ Assets/Scripts/Handle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class Handle : MonoBehaviour {
+    int angle = 90;
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0, 1, 0), angle);
+           
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0, 1, 0), -angle);
+        }
+    }
+}
---
+++ Assets/Scripts/Spin3.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin3 : MonoBehaviour
+{
+
+    private bool isSpinning = false;
+    private float spinCounter = 0;
+    private float spinStep = 360.0f / 4.0f;
+    public int value = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isSpinning)
+        {
+            isSpinning = !isSpinning;
+            spinCounter = Random.Range(170, 200);
+        }
+
+        if (isSpinning && spinCounter > 0)
+        {
+            transform.Rotate(new Vector3(0, 0, 1), spinStep);
+            --spinCounter;
+            value = (value + 1) % 4;
+        }
+        else if (isSpinning && spinCounter <= 0)
+        {
+            isSpinning = false;
+        }
+        if (isSpinning == false && Input.GetKeyDown(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), spinStep);
+        }
+    }
+}
